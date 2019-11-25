thomasfredericks/Bounce2

commit 01e6fc9398d3a59544c20d6d8000117d6dcd0246

Added a protected changed() function

src/Bounce2.h
@@ -194,6 +194,10 @@ class Bounce
     inline void unsetStateFlag(const uint8_t flag)  {state &= ~flag;}
     inline void toggleStateFlag(const uint8_t flag) {state ^= flag;}
     inline bool getStateFlag(const uint8_t flag)    {return((state & flag) != 0);}
+ 
+  protected:
+    bool Bounce::changed( ) { return getStateFlag(CHANGED_STATE); }
+
 };
 
 #endif

src/Bounce2.cpp
@@ -104,7 +104,7 @@ bool Bounce::update()
     
 #endif
 
-		return  getStateFlag(CHANGED_STATE); 
+		return  changed(); 
 
 }
 

commit 26e216c98d084e6a7f5d2c2bf703cf4463ec07af

Fix compilation error
libraries/Bounce2/src/Bounce2.h:209:10: error: extra qualification 'Bounce::' on member 'changed' [-fpermissive]
  209 |     bool Bounce::changed( ) { return getStateFlag(CHANGED_STATE); }
      |          ^~~~~~

Regression introduced in 01e6fc9398d3a59544c20d6d8000117d6dcd0246

Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

src/Bounce2.h
@@ -206,7 +206,7 @@ class Bounce
     inline bool getStateFlag(const uint8_t flag)    {return((state & flag) != 0);}
  
   public:
-    bool Bounce::changed( ) { return getStateFlag(CHANGED_STATE); }
+    bool changed( ) { return getStateFlag(CHANGED_STATE); }
 
 };
 